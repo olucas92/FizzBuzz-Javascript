1e5ce91bca5ada543820f172bd82c715f32fef0c
Oliver <o.lucas@live.com>
2015-01-20 13:05:12 +0000

    divisible by 3 done, divisible by 5 method written

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,24 @@
+describe("FizzBuzz", function() {
+
+  var fizzbuzz;
+
+  describe("knows when a number", function() {
+
+    it("is divisible by 3", function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it("is not divisible by 3", function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it("is divisible by 5", function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+  });
+
+});

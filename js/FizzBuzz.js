4f5173a0545804ad754e3d7df877985480bd538f
Oliver <o.lucas@live.com>
2015-01-20 13:09:03 +0000

    divisble by 15 method written

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -6,4 +6,8 @@ FizzBuzz.prototype.isDivisibleByThree = function(number) {
 
 FizzBuzz.prototype.isDivisibleByFive = function(number) {
   return number % 5 === 0;
+};
+
+FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+  return number % 15 === 0;
 }
